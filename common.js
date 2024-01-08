27552b66f03a45b8b46105d0660e36499d701d96
Partha Ghosh <112557191+parth721@users.noreply.github.com>
2024-01-08 13:10:40 +0530

    By Design
    Chrome extension

---
+++ common.js
@@ -0,0 +1,50 @@
+// Polyfill for Chrome
+// https://bugs.chromium.org/p/chromium/issues/detail?id=798169
+if (typeof globalThis.browser === "undefined") {
+    globalThis.browser = globalThis.chrome;
+  }
+  
+  /**
+   * @typedef {{
+   *   syncSettings: boolean,
+   *   thumbnailMode: 'hidden' | 'hidden-except-hover' | 'blurred' | 'solid-color' | 'normal',
+   *   disabledOnPages: {
+   *     results: boolean,
+   *     channel: boolean,
+   *     playlist: boolean,
+   *     watch: boolean,
+   *     subscriptions: boolean,
+   *     everywhere: boolean,
+   *   },
+   * }} Options
+   */
+  
+  /** @type {Options} */
+  const defaultOptions = {
+    syncSettings: true,
+    disabledOnPages: {
+      results: false,
+      channel: false,
+      playlist: false,
+      watch: false,
+      subscriptions: false,
+      everywhere: false,
+    },
+    thumbnailMode: 'hidden',
+  }
+  
+  /**
+   * @returns {Promise<Options>}
+   */
+  const loadOptions = async () => {
+    /** @type {boolean} */
+    const syncSettings = (await new Promise((resolve) => {
+      browser.storage.local.get(defaultOptions, resolve);
+    })).syncSettings ?? defaultOptions.syncSettings;
+  
+    const options = await new Promise((resolve) => {
+      browser.storage[syncSettings ? 'sync' : 'local'].get(defaultOptions, resolve);
+    })
+  
+    return { ...defaultOptions, ...options }
+  }
